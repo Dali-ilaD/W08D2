e07e5be4fb1fbf01a7a6313d0a3f468e6126a8cd
David Lai <pf.davidlai@gmail.com>
2023-03-21 15:11:05 -0700

    finished uniq

---
+++ phase_1_arrays.js
@@ -0,0 +1,18 @@
+// Array.prototype.uniq = function() {
+//    let newArr = [];
+   
+//         this.forEach(function(ele) {
+//             if(!newArr.includes(ele)){
+//                 newArr.push(ele);
+//             }
+//         }
+//         );
+        
+//      console.log(newArr);
+//      return newArr;
+// };
+
+// [1,2,2,3,3,3].uniq() 
+
+
+Array.prototype.twoSum
